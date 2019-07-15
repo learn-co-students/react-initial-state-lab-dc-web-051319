fb38dc4a71b44fac64962c46585053058ccaa740
Matt Williams <matt.seongjun@gmail.com>
2019-07-15 16:14:56 -0400

    Brain. Hurt

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,18 @@
-// your ImageSlider code here!
+import { Component } from "react";
+
+class ImageSlider extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+        return (
+            
+        )
+    }
+} 
+
+export default ImageSlider
